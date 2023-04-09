191b369cc97d945dac4f8dddbd6a4f0ac6ded0f6
treerspeaking <93274119+treerspeaking@users.noreply.github.com>
2023-04-09 17:54:01 +0700

    update main.py to properlly fit the new .csv

--- rcode/main.py
+++ rcode/main.py
@@ -1,22 +1,22 @@
 import csv
 
-with open('cpu-before.csv', newline='') as csvfile:
+with open('cpu-short.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     data_before =[]
     for row in reader:
         data_before.append(row)
 
-with open('cpu after.csv', newline='') as csvfile:
+with open('cpu-clean.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     data_after =[]
     for row in reader:
         data_after.append(row)
 
-print(data_before[0][6])
-print(data_after[0][6])
+print(data_before[4][10])
+print(data_after[4][10])
 for row in range(len(data_after)):
     for col in range(len(data_after[row])):
-        if(data_after[row][col] == 'NA') :
+        if(data_after[row][col] == 'NA' or data_after[row][col] == '') :
             if(data_before[row][col] == 'N/A' or data_before[row][col] == '' or data_before[row][col] == 'NA') :
                 continue
             else :
